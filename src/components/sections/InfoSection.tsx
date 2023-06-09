b5274cfe9f31ab4b3fc232abe468a47110844fe0
Artur Poffo <arturpoffoc@hotmail.com>
2023-06-08 21:24:30 -0300

    aviso

--- src/components/sections/InfoSection.tsx
+++ src/components/sections/InfoSection.tsx
@@ -4,6 +4,8 @@ export function InfoSection() {
   return (
     <section id="info" className="pb-16">
       <SectionTitle text="Informações" />
+
+      <h3 className="text-3xl text-orange-400">Em construção...</h3>
     </section>
   )
 }
